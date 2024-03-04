2d8fd0cae03f8286af6221b5f4f5e0b3631ae4ad
Eduard <edik01101997@mail.ru>
2024-03-04 12:14:08 +0300

    Initial commit

---
+++ src/test/java/com/example/iichatbot/IiChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iichatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IiChatBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
